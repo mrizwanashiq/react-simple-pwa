b1dbe8197b4aa5356ff6a0a22d3e91b67dbd73a8
mrizwanashiq <mrizwanashiq@outlook.com>
2024-08-21 17:15:51 +0500

    feat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-
-let count = 0;
+import "./App.css";
 const App = () => {
-    React.useEffect(() => {
-        if (count < 1) {
-            window.open("https://twitter.com/home", "_blank")
-            count += 1;
-        }
-    }, [])
+  const [count, setCount] = React.useState(0);
 
-
-    return (
-        <div className="main-container">
-
-        </div>
-    );
-}
+  return (
+    <div className="main-container">
+      {/* a simle screen showing data */}
+      <h1 style={{ color: count > 0 ? "green" : count < 0 ? "red" : "white" }}>
+        Counter App
+      </h1>
+      <h2 style={{ color: count > 0 ? "green" : count < 0 ? "red" : "white" }}>
+        {count}
+      </h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
 
 export default App;
